c4acda65d8f5f368a0a4cb7405f51c0ce74ecd01
Bazyli Cyran <bazyli.cyran@gmail.com>
2022-02-19 15:04:04 +0100

    feat: Install CLI dependencies as 'cli' extra

--- src/philipstv/__main__.py
+++ src/philipstv/__main__.py
@@ -1,4 +1,16 @@
-from .cli import cli
+import sys
+
+
+def wrapped_cli() -> None:
+    try:
+        from .cli import cli
+    except ModuleNotFoundError:
+        print("CLI dependencies missing!", file=sys.stderr, flush=True)
+        print("Please install the package as 'philipstv[cli]'.", file=sys.stderr, flush=True)
+        sys.exit(1)
+    else:
+        cli()
+
 
 if __name__ == "__main__":
-    cli()
+    wrapped_cli()
