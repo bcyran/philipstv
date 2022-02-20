fb76f98ee988dc6b471cbb527ac650edf4192f7d
Bazyli Cyran <bazyli.cyran@gmail.com>
2022-02-20 17:49:37 +0100

    refactor: Use contextmanager instead of decorator for wrapping HTTP exceptions
    Decorator cannot be properly typed without ParamSpec which is available
    only in Python 3.10.

--- src/philipstv/tv.py
+++ src/philipstv/tv.py
@@ -1,6 +1,6 @@
 import logging
-from functools import wraps
-from typing import Any, Callable, Optional, ParamSpec, TypeVar
+from contextlib import contextmanager
+from typing import Any, Iterator, Optional
 from urllib.parse import urljoin
 
 import urllib3
@@ -18,22 +18,15 @@ urllib3.disable_warnings(InsecureRequestWarning)  # type: ignore
 _LOGGER = logging.getLogger(__name__)
 
 
-_T = TypeVar("_T")
-_P = ParamSpec("_P")
-
-
-def _wrap_http_exceptions(func: Callable[_P, _T]) -> Callable[_P, _T]:
-    @wraps(func)
-    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _T:
-        try:
-            return func(*args, **kwargs)
-        except HTTPError as exc:
-            status_code = None
-            if exc.response is not None:
-                status_code = exc.response.status_code
-            raise PhilipsTVError(status_code) from exc
-
-    return wrapper
+@contextmanager
+def _wrap_http_exceptions() -> Iterator[None]:
+    try:
+        yield
+    except HTTPError as exc:
+        status_code = None
+        if exc.response is not None:
+            status_code = exc.response.status_code
+        raise PhilipsTVError(status_code) from exc
 
 
 class PhilipsTV(PhilipsTVInterface):
@@ -48,20 +41,20 @@ class PhilipsTV(PhilipsTVInterface):
     def set_auth(self, auth: Optional[Credentials]) -> None:
         self._session.auth = HTTPDigestAuth(*auth) if auth else None
 
-    @_wrap_http_exceptions
     def post(self, path: str, payload: Any = None) -> Any:
         _LOGGER.debug("Request: POST %s %s", path, payload)
-        response = self._session.post(urljoin(self.url, path), json=payload)
-        response.raise_for_status()
+        with _wrap_http_exceptions():
+            response = self._session.post(urljoin(self.url, path), json=payload)
+            response.raise_for_status()
         response_body = response.json() if response.content else None
         _LOGGER.debug("Response: %s %s", response.status_code, response_body)
         return response_body
 
-    @_wrap_http_exceptions
     def get(self, path: str) -> Any:
         _LOGGER.debug("Request: GET %s", path)
-        response = self._session.get(urljoin(self.url, path))
-        response.raise_for_status()
+        with _wrap_http_exceptions():
+            response = self._session.get(urljoin(self.url, path))
+            response.raise_for_status()
         response_body = response.json() if response.content else None
         _LOGGER.debug("Response: %s %s", response.status_code, response_body)
         return response_body
