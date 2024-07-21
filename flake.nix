fac2dd6608fb0df1f90e944be6bdafccd6b5f594
Bazyli Cyran <bazyli@cyran.dev>
2024-07-21 19:22:55 +0200

    chore: Setup flake devenv

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            ({pkgs, ...}: {
+              languages.python = {
+                enable = true;
+                poetry = {
+                  enable = true;
+                  install = {
+                    enable = true;
+                    installRootPackage = true;
+                    extras = ["cli"];
+                  };
+                  activate.enable = true;
+                };
+              };
+              packages = with pkgs; [
+                pyright
+              ];
+            })
+          ];
+        };
+      });
+  };
+}
